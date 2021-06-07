a2db9e259d020664eef41da37e6ed4cf0391d244
Silas <123zsl@163.com>
2021-06-07 23:38:06 +0800

    added the callback prop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+function addTask(name) {
+  alert(name);
+}
 
 function App(props) {
 
@@ -14,40 +17,10 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      {/* <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form> */}
-      <Form />
+
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        {/* <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button> */}
+       
         <FilterButton />
         <FilterButton />
         <FilterButton />
